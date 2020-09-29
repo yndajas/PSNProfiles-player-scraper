d82cdd80b345df2d500585d73f624b3b25fcc921
Ynda Jas <40244233+yndajas@users.noreply.github.com>
2020-09-29 04:39:47 +0100

    Shift to local environment
    Inconsequential formatting and other minor changes as a result of shift to local environment

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -2,17 +2,17 @@ class Scraper
   BASE_PATH = "https://psnprofiles.com/"
 
   def self.scrape_profile_page(psn_id)
-    
+
     profile_data = Nokogiri::HTML(open(BASE_PATH + psn_id))
-    
+
     player = {}
-    
+
     player[:psn_id] = psn_id
-    
+
     # get row of eight stats near top of profile
-    
+
     stats_flex = profile_data.css("span.stat.grow")
-    
+
     stats_flex_data = []
 
     stats_flex.each do |stat|
@@ -22,11 +22,11 @@ class Scraper
     stats_flex_data.delete_at(5) # delete (unwanted) profile views count
 
     stats_flex_data_keys = [:games_played, :completed_games, :completion_rate, :unearned_trophies, :trophies_per_day, :world_rank, :country_rank]
-    
+
     stats_flex_data.each_with_index do |stat, i|
       player[stats_flex_data_keys[i]] = stat
     end
-    
+
     player
   end
 end
