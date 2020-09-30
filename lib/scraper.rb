34071b6dda59cc143c2cbc2c0f569b030cb563ea
Ynda Jas <40244233+yndajas@users.noreply.github.com>
2020-09-30 15:48:46 +0100

    Additional scrapes
    - total platinums, golds, silvers, bronzes
    - up to 12 recent trophies (trophy, game, description)
    
    + additional comments including planned data for recent games

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -7,6 +7,8 @@ class Scraper
 
     player = {}
 
+    # get data from top row (including mouseover extras)
+
     player[:psn_id] = profile_data.css("span.username").text.strip #this gets the username with the correct capitalisation
 
     player[:comment] = profile_data.css("span.comment").text if profile_data.css("span.comment").length > 0
@@ -21,7 +23,15 @@ class Scraper
 
     player[:total_trophies] = profile_data.css("li.total").text.strip
 
-    # get row of eight stats near top of profile
+    player[:total_platinums] = profile_data.css("li.platinum").text.strip
+
+    player[:total_golds] = profile_data.css("li.gold").text.strip
+
+    player[:total_silvers] = profile_data.css("li.silver").text.strip
+
+    player[:total_bronzes] = profile_data.css("li.bronze").text.strip
+
+    # get data from second row (of eight stats near top of profile)
 
     stats_flex = profile_data.css("span.stat.grow")
 
@@ -38,6 +48,26 @@ class Scraper
     stats_flex_data.each_with_index do |stat, i|
       player[stats_flex_data_keys[i]] = stat
     end
+
+    # get recent trophies
+
+    recent_trophies = []
+
+    recent_trophies_scrape = profile_data.css("ul.recent-trophies.flex li")
+
+    recent_trophies_scrape.length.times {recent_trophies << {}} # create empty hashes for each of the up to 12 recent trophies - I use the #length instead of 12.times so that it works for players with less than 12 total trophies
+
+    recent_trophies_scrape.each_with_index do |trophy, i|
+      recent_trophies[i][:trophy] = trophy.css("div.ellipsis a.title").text.strip
+      recent_trophies[i][:game] = trophy.css("div.ellipsis span.small_info_green a").text.strip
+      recent_trophies[i][:description] = trophy.css("div.ellipsis span")[0].text.strip
+    end
+
+    player[:recent_trophies] = recent_trophies
+
+    # get recent games
+
+    ## game, N of N trophies, latest trophy date, platinum/completion time (if platinumed/completed), platform, platinumed (check which icon shows), g/s/b, completion percentage, platinum rarity, completion rate (if it exists, i.e. if it's not 1:1 with platinum rate/if there's DLC or no platinum)
 
     binding.pry
 
