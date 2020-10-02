f4fdc4e18530a3deb4de3b1e9b80dd2db4a925c6
Ynda Jas <40244233+yndajas@users.noreply.github.com>
2020-10-02 04:34:26 +0100

    Prepping Player class
    Changed :psn_id from accessor to reader and added notes for building out the class and its functionality

--- lib/player.rb
+++ lib/player.rb
@@ -1,16 +1,20 @@
 class Player
-  attr_accessor :psn_id
-  
+  attr_reader :psn_id
+
+  # add attr_readers for all the keys in the scraped player_data hash, and add reader methods to get the sub-hashes/attributes
+  # use meta-programming (#send) in #initialize to populate all the instance variables
+  # add methods for putting (or returning?) each of the five collections of data outlined in the README in an easily readable format
+
   @@all = []
-  
+
   def initialize(player_data)
     @psn_id = psn_id
     @@all << self
   end
-  
+
   def self.all
     @@all
   end
-  
+
   # delete method if the PSN ID is not scrapable?
 end
